9b8d9172d56ebad9f8a24e6a6b74c15db174b4f6
Matias Leitner <leitnermatias22@gmail.com>
2022-02-09 15:00:15 -0300

    Constantly update titles

--- src/views/Titles.vue
+++ src/views/Titles.vue
@@ -25,7 +25,8 @@
     </div>
 
     <!-- Grid -->
-    <div v-if="titles.length > 0" class="w-11/12 flex flex-col gap-1 shadow-2xl border-4 border-gray-300 p-2 bg-gray-600 bg-opacity-5">
+    <div v-if="titles.length > 0" class="w-11/12 flex flex-col gap-1 shadow-2xl border-4 border-gray-300 p-2 bg-blue-300 bg-opacity-15">
+        <span v-if="titlesCount !== null" class="text-xl text-blue-900 text-opacity-15 mb-4 border-b-4 border-blue-900 border-opacity-10 text-center">Cantidad de noticias encontradas: {{titlesCount}}</span>
         <div class="w-full h-80 flex flex-row items-center shadow-xl text-center border-2 border-gray-900 border-opacity-30 max-h-40 opacity-90 hover:opacity-100" v-for="title in titles" :key="title.id">
             <img class="h-full w-96 border-blue-700 border-2 border-opacity-25 min-w-0 rounded-sm" :src="title.img">
             <div class="flex flex-col w-40 h-full text-sm bg-blue-600 bg-opacity-20 border-l-2 border-r-2 border-gray-900 rounded-sm p-2">
@@ -55,6 +56,7 @@ export default {
         return {
             loading: false,
             showFilter: true,
+            titlesCount: null,
             titles: [],
             newspapers: [],
             filter: {
@@ -70,7 +72,7 @@ export default {
             fetch(`${process.env.API_URL}/titles?newspaper=${this.filter.newspaper}&header=${this.filter.header}`)
             .then(res => res.json())
             .catch(error => console.log(error))
-            .then(data => this.titles = data.result)
+            .then(data => {this.titles = data.result; this.titlesCount = data.titlesCount !== undefined ? data.titlesCount : null})
             .catch(error => console.log(error))
             this.loading = false;
         },
@@ -104,6 +106,10 @@ export default {
     mounted() {
         this.getNewspapers()
         this.search()
+
+        window.setInterval(() => {
+            this.search()
+        }, 5000)
     }
 
 
